e1afb441810f77ce55a9a961eacc579508ce0c88
lelonea <leloneya@gmail.com>
2024-07-24 22:18:23 +0400

    fix cart

--- src/pages/Cart/CartItem.jsx
+++ src/pages/Cart/CartItem.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography, Paper, Box, styled } from "@mui/material"
+import {Button, Typography, Paper, Box, styled, Stack} from "@mui/material"
 import { removeFromCart } from "../../redux/slice/CartSlice"
 import Counter from "../../components/organisms/Counter"
 import { useDispatch } from 'react-redux'
@@ -34,48 +34,49 @@ const CartItem = ({item, updateCount}) => {
   }
   
   return (  
-    <Paper sx={{display: 'flex', 
-              flexDirection: 'row', 
-              alignItems: 'center', 
-              gap: '30px', 
-              padding: '10px', 
-              width: '100%',
-              height: '180px',
-              overflow: 'hidden',
-              borderRadius: '10px'}}
-              elevation={3}
-              variant="outlined">
+    <Stack sx={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: '30px',
+        padding: '10px',
+        height: '180px',
+        overflow: 'hidden',
+        borderRadius: '10px',
+        border: '1px solid #e0e0e0',
+        justifyContent: 'space-between',
+    }}>
       <Image src={`http://localhost:3333/${item.image}`} alt={item.title}/>
-      <Box sx={{display: 'flex', flexDirection: 'column', gap: isSmallScreen? '10px' : '30px'}}>
-        <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
+      <Stack sx={{flexDirection: 'column', gap: isSmallScreen? '10px' : '30px', alignItems: 'flex-start', flex: 1}}>
+        <Stack sx={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
           <StyledNavLink to={`/products/${item.id}`}>
-            <Typography variant={isSmallScreen? 'subtitle1' : 'h5'} 
+            <Typography variant={isSmallScreen? 'subtitle1' : 'h5'}
                 sx={{ lineHeight: '1.2', fontWeight: '300'}}>
                 {item.title}</Typography>
           </StyledNavLink>
           <Button variant="text" 
                   onClick={() => deleteItem(item.id)} 
-                  sx={{width: '10px !important',
-                  height: '10px !important',
-                  padding: 0,
-                  marginRight: '0 !important',
-                  marginLeft: 'auto',
-                  '&:hover': {color: 'rgba(255, 0, 0)'}}}>x</Button>
-        </Box>
-        <Box sx={{display: 'flex', flexDirection: isSmallScreen? 'column' : 'row', gap: isSmallScreen? '5px' : '30px', alignItems: 'center'}}>
+                  sx={{
+                    width: '10px !important',
+                    height: '10px !important',
+                    padding: 0,
+                    marginRight: '0 !important',
+                    marginLeft: 'auto',
+                    '&:hover': {color: 'rgba(255, 0, 0)'}}}>x</Button>
+        </Stack>
+        <Stack sx={{flexDirection: isSmallScreen? 'column' : 'row', gap: isSmallScreen? '5px' : '30px', alignItems: 'center'}}>
           <Counter item={item} cart/>
           {item.discont_price ? (
-          < Box sx={{display: 'flex', flexDirection: 'row', gap: '5px', alignItems: 'center'}}>
+          <Stack sx={{flexDirection: 'row', gap: '5px', alignItems: 'center'}}>
           <Typography variant='h4' sx={{ fontWeight: 'bold' }}>${item.discont_price}</Typography>
           <Typography sx={{ color: 'rgba(139, 139, 139, 1)', textDecoration: 'line-through', fontSize: '20px' }}>${item.price}</Typography>
           
-          </Box>
+          </Stack>
           ) : (
           <Typography variant='h4' sx={{ fontWeight: 'bold' }}>${item.price}</Typography>
           )}
-        </Box>
-      </Box>
-    </Paper>
+        </Stack>
+      </Stack>
+    </Stack>
   )
 }
 export default CartItem
